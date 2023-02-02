21b8e0155575f2c7a2639293226fe8f1856213b4
lordmike007 <manujasan23@gmail.com>
2023-02-02 21:55:29 +0530

    added subscribe to notification functionalities

--- src/routes/chat/components/ShowMutual/index.svelte
+++ src/routes/chat/components/ShowMutual/index.svelte
@@ -1,16 +1,57 @@
 <script lang="ts">
 	import Borders from '$lib/components/Borders/index.svelte';
+	import Button from '$lib/components/Button/index.svelte';
+	import { fetchSigner } from '@wagmi/core';
+	import { networkConnectionData } from '$lib/stores';
+	import * as PushAPI from '@pushprotocol/restapi';
 	export let mutualInterests: Array<string>;
 	export let className: string;
+	let receivingNotifications = false;
+
+	const optInToNotifications = async () => {
+		let signer = await fetchSigner();
+		await PushAPI.channels.subscribe({
+			signer: signer,
+			channelAddress: 'eip155:80001:0xEFDeb468C681EEF5Df33E58920B3027B52d525D7', // channel address in CAIP
+			userAddress: `eip155:80001:${$networkConnectionData.myAddress}`, // user address in CAIP
+			verifyingContractAddress: '0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa',
+			onSuccess: () => {
+				console.log('opt in success');
+			},
+			onError: () => {
+				console.error('opt in error');
+			},
+			env: 'staging'
+		});
+		receivingNotifications = true;
+	};
+
+	const optOutFromNotifications = async () => {
+		let signer = await fetchSigner();
+		await PushAPI.channels.unsubscribe({
+			signer: signer,
+			channelAddress: 'eip155:80001:0xEFDeb468C681EEF5Df33E58920B3027B52d525D7', // channel address in CAIP
+			userAddress: `eip155:80001:${$networkConnectionData.myAddress}`, // user address in CAIP
+			verifyingContractAddress: '0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa',
+			onSuccess: () => {
+				console.log('opt out success');
+			},
+			onError: () => {
+				console.error('opt out error');
+			},
+			env: 'staging'
+		});
+		receivingNotifications = false;
+	};
 
 	$: if (mutualInterests.length < 1) {
 		mutualInterests = ['void'];
 	}
 </script>
 
-<div class={className}>
+<div class={[className, 'flex justify-between items-center'].join(' ')}>
 	<div
-		class="m-auto md:ml-0 md:mr-auto mt-4 p-4 xl:py-8 flex items-center gap-4 relative h-[57.6px] xl:h-[72px] max-w-[23rem] xl:max-w-[30rem] border-[3px] border-solid border-[#797979]"
+		class="m-auto md:ml-0 md:mr-auto  mt-4 p-4 xl:py-8 flex items-center gap-4 relative h-[57.6px] xl:h-[72px] max-w-[23rem] xl:max-w-[30rem] border-[3px] border-solid border-[#797979]"
 	>
 		<span>Mutual:</span>
 		<div class="relative">
@@ -37,4 +78,11 @@
 			</div>
 		</div>
 	</div>
+	<Button
+		className="mr-0 hidden md:block"
+		on:click={() => {
+			if (receivingNotifications) optOutFromNotifications();
+			else optInToNotifications();
+		}}>{receivingNotifications ? 'Disable Notifications' : 'Receive Notifications'}</Button
+	>
 </div>
